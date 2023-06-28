b3c9d71c3c044b61f220eb3a5c544ddb9b25a0d8
James Doss-Gollin <james.dossgollin@gmail.com>
2023-06-28 14:22:40 -0500

    fix typo

--- scripts/get_data.jl
+++ scripts/get_data.jl
@@ -6,7 +6,7 @@ using Dates
 RDF_FOLDER = "/home/jd82/RDF/jd82" # only the Doss-Gollin Group will be able to access this!
 
 # define the datasets to use
-datasets = [NexradDataset(joinpath(RDF_FOLDER, "NEXRAD")]
+datasets = [NexradDataset(joinpath(RDF_FOLDER, "NEXRAD"))]
 
 for ds in datasets
     @info "Building dataset..."
